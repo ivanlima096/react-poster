556a4e4f8691ce3c99ab78c08d364acbef261954
ivanlima096 <ivanlima096@gmail.com>
2023-04-26 15:01:26 -0300

    uso de props com diferentes filmes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Card from "./components/Card"
+import posterImg from "./assets/img.jpg"
+import posterImg2 from "./assets/img2.jpg"
+import posterImg3 from "./assets/img3.jpg"
 
 export default function App() {
   return (
     <div className="app">
-      <Card />
+      <Card title="Pôster: Star Wars (1977)" img={posterImg} />
+      <Card title="Pôster: Empire Strikes Back (1980)" img={posterImg2} />
+      <Card title="Pôster: Return of the Jedi (1983)" img={posterImg3} />
     </div>
   )
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,15 +1,16 @@
-import posterImg from "../../assets/img.jpg"
-import styles from "./styles.module.css"
 
-export default function Card() {
+import styles from "./styles.module.css"
+import Button from "../Button"
+
+export default function Card(props) {
   return (
     <div className={styles.containerFather} >
       <div className={styles.container}>
-        <img className={styles.poster} src={posterImg} alt="imagem poster" />
+        <img className={styles.poster} src={props.img} alt={props.title} />
         <div>
-          <h2>Poster do Filme Star Wars</h2>
+          <h2>{props.title}</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus illum deserunt quam! Quae labore amet nesciunt molestias dolorem! Deleniti atque est rerum at suscipit laborum officia labore iusto eius aut sint tempora, magnam hic? Laudantium tempora qui sunt dicta nesciunt.</p>
-          <button>Comprar Agora</button>
+          <Button />
         </div>
       </div>
     </div>
